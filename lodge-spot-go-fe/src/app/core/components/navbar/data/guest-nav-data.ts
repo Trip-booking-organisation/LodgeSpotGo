60e3eb1621849f32bd6fa4fb405b4dd50b38db74
DjordjeVuckovic <vuckovic.ra8.2019@uns.ac.rs>
2023-05-13 15:40:38 +0200

    keycloak integration

---
+++ lodge-spot-go-fe/src/app/core/components/navbar/data/guest-nav-data.ts
@@ -0,0 +1,18 @@
+export const guestNavData = [
+  {
+    label: 'Home',
+    routerLink: ''
+  },
+  {
+    label: 'Flights',
+    routerLink: ''
+  },
+  {
+    label: 'About',
+    routerLink: ''
+  },
+  {
+    label: 'Seats',
+    routerLink: ''
+  }
+]
---
+++ lodge-spot-go-fe/src/app/core/components/navbar/data/no-register-user-nav-data.ts
@@ -0,0 +1,14 @@
+export const noRegisterUserNavData = [
+  {
+    label: 'Home',
+    routerLink: '/'
+  },
+  {
+    label: 'About',
+    routerLink: ''
+  },
+  {
+    label: 'Seats',
+    routerLink: ''
+  }
+]
