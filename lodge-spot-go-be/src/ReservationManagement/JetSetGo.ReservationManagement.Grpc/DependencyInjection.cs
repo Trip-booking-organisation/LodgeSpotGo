f26c5fec0cafbe09b44f24c1079c572dada1f219
ZakicAleksa <93937431+ZakicAleksa@users.noreply.github.com>
2023-05-13 16:30:09 +0200

    reservation backend added

---
+++ lodge-spot-go-be/src/ReservationManagement/JetSetGo.ReservationManagement.Grpc/DependencyInjection.cs
@@ -0,0 +1,12 @@
+﻿using JetSetGo.ReservationManagement.Grpc.Mapping;
+
+namespace JetSetGo.ReservationManagement.Grpc;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddPresentation(this IServiceCollection services)
+    {
+        services.AddAutoMapper(typeof(MappingConfiguration));
+        return services;
+    }
+}
---
+++ lodge-spot-go-be/src/ReservationManagement/JetSetGo.ReservationManagement.Infrastructure/Persistence/Repository/ReservationRepository.cs
@@ -0,0 +1,28 @@
+﻿using JetSetGo.ReservationManagement.Application.Common.Persistence;
+using JetSetGo.ReservationManagement.Domain.Reservation;
+using JetSetGo.ReservationManagement.Infrastructure.Persistence.Settings;
+using Microsoft.Extensions.Options;
+using MongoDB.Driver;
+namespace JetSetGo.ReservationManagement.Infrastructure.Persistence.Repository;
+using Microsoft.Extensions;
+using MongoDB.Driver;
+
+public class ReservationRepository : IReservationRepository
+{
+    private readonly IMongoCollection<Reservation> _reservationCollection;
+    public ReservationRepository(IOptions<DatabaseSettings> dbSettings)
+    {
+        var client = new MongoClient(dbSettings.Value.ConnectionString);
+        var mongo = client.GetDatabase(dbSettings.Value.DatabaseName);
+        _reservationCollection = mongo.GetCollection<Reservation>(dbSettings.Value.CollectionName);
+    }
+    
+    
+    public async Task<List<Reservation>> GetAllAsync(CancellationToken cancellationToken = default) =>
+        await _reservationCollection.Find(_=> true).ToListAsync(cancellationToken);
+
+    public async Task CreateAsync(Reservation reservation)
+    {
+        await _reservationCollection.InsertOneAsync(reservation);
+    }
+}
