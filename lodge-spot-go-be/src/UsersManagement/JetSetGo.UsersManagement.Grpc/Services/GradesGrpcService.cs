01c86073cd99185c706a05b4416bee5e39311c31
Aleksa <aleksazakica@gmail.com>
2023-06-14 19:54:10 +0200

    update delete and get hosts grades aded to backend

--- lodge-spot-go-be/src/UsersManagement/JetSetGo.UsersManagement.Grpc/Services/GradesGrpcService.cs
+++ lodge-spot-go-be/src/UsersManagement/JetSetGo.UsersManagement.Grpc/Services/GradesGrpcService.cs
@@ -5,6 +5,7 @@ using Grpc.Net.Client;
 using JetSetGo.UserManagement.Grpc;
 using JetSetGo.UsersManagement.Application.Common.Persistence;
 using JetSetGo.UsersManagement.Domain.HostGrade.Entities;
+using JetSetGo.UsersManagement.Domain.User.Entities;
 using JetSetGo.UsersManagement.Grpc.Client;
 using JetSetGo.UsersManagement.Grpc.Dto;
 
@@ -20,6 +21,68 @@ public class GradesGrpcService
         _hostGradeRepository = gradeRepository;
         _mapper = mapper;
         _reservationClient = reservationClient;
+    }
+
+    public async Task<DeleteHostGradeResponse> DeleteHostGrade(DeleteHostGradeRequest request)
+    {
+        var grade = await CheckAndFindGrade(request.gradeId,request.guestId);
+
+        await _hostGradeRepository.DeleteGrade(request.gradeId);
+        return new DeleteHostGradeResponse { Success = true };
+    }
+
+    public async Task<UpdateHostGradeResponse> UpdateHostGrade(UpdateHostGradeRequest request)
+    {
+        var grade = await CheckAndFindGrade(request.Id,request.GuestId);
+
+        grade.Number = request.Number;
+        await _hostGradeRepository.UpdateGrade(grade);
+        return new UpdateHostGradeResponse { Success = true };
+    }
+
+    private async Task<HostGrade> CheckAndFindGrade(Guid id,Guid gusetId)
+    {
+        var grade = await _hostGradeRepository.GetById(id);
+        if (grade is null)
+        {
+            throw new Exception("Grade is not found");
+        }
+
+        if (gusetId != grade.GuestId)
+        {
+            throw new Exception("This is not your host grade, so you cant delete it.");
+        }
+
+        return grade;
+    }
+    
+
+    public async Task<GetGradesByHostResponse> GetGradesByHost(GetGradesByHostRequest request)
+    {
+        var grades =await  _hostGradeRepository.GetAllByHost(request.HostId);
+        if (grades is null)
+        {
+            throw new Exception("No grades for selected host.");
+        }
+
+        return new GetGradesByHostResponse
+        {
+            HostGrades = grades
+        };
+    }
+    
+    public async Task<GetGradesByHostResponse> GetGradesByGuest(GetGradesByGuestRequest request)
+    {
+        var grades =await  _hostGradeRepository.GetAllByGuest(request.GuestId);
+        if (grades is null)
+        {
+            throw new Exception("No grades for selected guest.");
+        }
+
+        return new GetGradesByHostResponse
+        {
+            HostGrades = grades
+        };
     }
 
     public async Task<HostGradeResponse> CreateGradeForHost(HostGradeRequest request)
--- lodge-spot-go-be/src/UsersManagement/JetSetGo.UsersManagement.Infrastructure/Persistence/Repository/HostGradeRepository.cs
+++ lodge-spot-go-be/src/UsersManagement/JetSetGo.UsersManagement.Infrastructure/Persistence/Repository/HostGradeRepository.cs
@@ -21,4 +21,26 @@ public class HostGradeRepository:IHostGradeRepository
     {
         await _hostGradeCollection.InsertOneAsync(grade);
     }
+
+    public async Task<HostGrade> GetById(Guid id, CancellationToken cancellationToken = default) =>
+        await _hostGradeCollection.Find(x => x.Id == id)
+            .FirstOrDefaultAsync(cancellationToken);
+
+    public async Task DeleteGrade(Guid id)
+    {
+        var filter = Builders<HostGrade>.Filter.Eq(g => g.Id, id);
+        await _hostGradeCollection.DeleteOneAsync(filter);
+    }
+
+    public async Task UpdateGrade(HostGrade grade)
+    {
+        await _hostGradeCollection.ReplaceOneAsync(g => g.Id == grade.Id, grade);
+    }
+
+    public async Task<List<HostGrade>> GetAllByGuest(Guid guestId) =>
+        await _hostGradeCollection.Find(g => g.GuestId == guestId).ToListAsync();
+
+    public async Task<List<HostGrade>> GetAllByHost(Guid hostId) =>
+        await _hostGradeCollection.Find(g => g.HostId == hostId).ToListAsync();
+
 }
